ceacc86fefb667c3ae6e4ede694847e0b9db89ca
Iann-rst <iannrst11@gmail.com>
2022-09-13 18:40:18 -0300

    Feat: Home Page

---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,32 @@
+import { Image, View, FlatList } from 'react-native';
+
+import { styles } from './styles';
+
+
+import logoImg from '../../assets/logo-nlw-esports.png';
+import { Heading } from '../../components/Heading';
+import { GameCard } from '../../components/GameCard';
+
+import { GAMES } from '../../utils/games';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={logoImg}
+        style={styles.logo}
+      />
+
+      <Heading title="Encontre seu duo!" subtitle="Selecione o game que deseja jogar..." />
+
+      <FlatList
+        contentContainerStyle={styles.contentList}
+        data={GAMES}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (<GameCard data={item} />)}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      />
+    </View>
+  );
+}
