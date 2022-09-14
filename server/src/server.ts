62655ff53049369fcb23e5966df31a9f6b5964e0
Iann-rst <iannrst11@gmail.com>
2022-09-14 19:31:37 -0300

    Feat: Criação das rotas de requisição ao BD

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,14 @@
-import express, { response } from 'express'
+import express, { query } from 'express'
+import { PrismaClient } from '@prisma/client';
+import { convertHourStringToMinutes } from './utils/convert-hour-string-to-minutes';
+import { convertMinuteToHourString } from './utils/convert-minutes-to-hour-string';
 
 const app = express();
+app.use(express.json()); //permite o express a entender informações em json 
+
+const prisma = new PrismaClient({
+  log: ['query']
+});
 
 // www.minhaapi.com/ads
 
@@ -16,41 +24,93 @@ const app = express();
 
 
 //rota de listagem de games
-app.get('/games', (request, response) => {
-  return response.json([]);
+app.get('/games', async (request, response) => {
+
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true
+        }
+      }
+    }
+  })
+
+  return response.json(games);
 })
 
 
-//rota para criação de anúncios
-app.post('/ads', (request, response) => {
-  return response.status(201).json([]);
+//rota para criação de anúncios (um anuncio sempre tem um jogo)
+app.post('/games/:id/ads', async (request, response) => {
+  const gameId = request.params.id;
+  const body = request.body;
+
+  const ad = await prisma.ad.create({
+    data: {
+      gameId,
+      name: body.name,
+      yearsPlaying: body.yearsPlaying,
+      discord: body.discord,
+      weekDays: body.weekDays.join(','),
+      hourStart: convertHourStringToMinutes(body.hourStart),
+      hourEnd: convertHourStringToMinutes(body.hourEnd),
+      useVoiceChannel: body.useVoiceChannel,
+    }
+  })
+
+  return response.status(201).json(ad);
 })
 
 
 //rota para listar os anúncios de uma game especifico
-app.get('/games/:id/ads', (req, res) => {
-  //const gameId = req.params.id;
+app.get('/games/:id/ads', async (req, res) => {
+  const gameId = req.params.id;
 
+  const ads = await prisma.ad.findMany({
+    select: {
+      id: true,
+      name: true,
+      yearsPlaying: true,
+      weekDays: true,
+      hourStart: true,
+      hourEnd: true,
+      useVoiceChannel: true,
+    },
+    where: {
+      gameId,
+    },
+    orderBy: {
+      createdAt: 'desc'
+    }
+  })
 
-  return res.json([
-    {
-      id: 1, name: 'Anúncio 1'
-    },
-    {
-      id: 2, name: 'Anúncio 2'
-    },
-    {
-      id: 3, name: 'Anúncio 3'
-    },
-    {
-      id: 4, name: 'Anúncio 4'
-    },
-  ])
+  return res.json(ads.map(ad => {
+    return {
+      ...ad,
+      weekDays: ad.weekDays.split(','),
+      hourStart: convertMinuteToHourString(ad.hourStart),
+      hourEnd: convertMinuteToHourString(ad.hourEnd),
+
+    }
+  }))
 })
 
 //Buscar o discord pelo id do anúncio
-app.get('/ads/:id/discord', (request, response) => {
-  return response.json([])
+app.get('/ads/:id/discord', async (request, response) => {
+  const adId = request.params.id;
+
+  const ad = await prisma.ad.findUniqueOrThrow({
+    select: {
+      discord: true,
+    },
+    where: {
+      id: adId
+    }
+  })
+
+  return response.json({
+    discord: ad.discord,
+  })
 })
 
 app.listen(3333);
