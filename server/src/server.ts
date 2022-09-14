73a10bfe0fd0d5a29b74b9a940e663e678e8f5cc
Iann-rst <iannrst11@gmail.com>
2022-09-14 16:47:29 -0300

    Feat: back-end

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,37 @@
-import express from 'express'
+import express, { response } from 'express'
 
 const app = express();
 
 // www.minhaapi.com/ads
 
-app.get('/ads', (req, res) => {
+
+/* 
+  HTTP methods / API RESTful / HTTP Codes
+  GET, POST, PUT, PATCH, DELETE
+
+  Query: localhost:3333/ads?page=2
+  Route: localhost:3333/ads/5
+  Body: as informações passadas são usadas na requisição
+*/
+
+
+//rota de listagem de games
+app.get('/games', (request, response) => {
+  return response.json([]);
+})
+
+
+//rota para criação de anúncios
+app.post('/ads', (request, response) => {
+  return response.status(201).json([]);
+})
+
+
+//rota para listar os anúncios de uma game especifico
+app.get('/games/:id/ads', (req, res) => {
+  //const gameId = req.params.id;
+
+
   return res.json([
     {
       id: 1, name: 'Anúncio 1'
@@ -19,6 +46,11 @@ app.get('/ads', (req, res) => {
       id: 4, name: 'Anúncio 4'
     },
   ])
+})
+
+//Buscar o discord pelo id do anúncio
+app.get('/ads/:id/discord', (request, response) => {
+  return response.json([])
 })
 
 app.listen(3333);
