2cbaa6b3b7139c5f135849e5214b769be8e4c843
Iann-rst <iannrst11@gmail.com>
2022-09-15 19:14:36 -0300

    Feat: Página de game

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -2,19 +2,20 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import { useRoute, useNavigation } from '@react-navigation/native';
 import { Background } from '../../components/Background';
 import { Entypo } from '@expo/vector-icons'
-import { Image, TouchableOpacity, View } from 'react-native';
+import { FlatList, Image, TouchableOpacity, View } from 'react-native';
 
 import { GameParams } from '../../@types/navigation';
 import { Heading } from '../../components/Heading';
-import { DuoCard } from '../../components/DuoCard'
+import { AdsProps, DuoCard } from '../../components/DuoCard'
 
 
 import logoImg from '../../assets/logo-nlw-esports.png'
 import { THEME } from '../../theme';
 import { styles } from './styles';
-
+import { useEffect, useState } from 'react';
 
 export function Game() {
+  const [duos, setDuos] = useState<AdsProps[]>([]);
   const route = useRoute();
   const game = route.params as GameParams;
 
@@ -24,6 +25,13 @@ export function Game() {
   function handleGoBack() {
     navigation.goBack()
   }
+
+
+  useEffect(() => {
+    fetch(`http://10.0.0.133:3333/games/${game.id}/ads`)
+      .then(response => response.json())
+      .then(data => setDuos(data))
+  }, [])
 
   return (
     <Background>
@@ -49,7 +57,21 @@ export function Game() {
 
         <Heading title={game.title} subtitle="Conecte-se e comece a jogar!" />
 
-        <DuoCard />
+        <FlatList
+          data={duos}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <DuoCard
+              data={item}
+              onConnect={() => { }}
+            />
+          )}
+          horizontal
+          style={styles.containerList}
+          contentContainerStyle={styles.contentList}
+          showsHorizontalScrollIndicator={false}
+        />
+
 
       </SafeAreaView>
     </Background>
