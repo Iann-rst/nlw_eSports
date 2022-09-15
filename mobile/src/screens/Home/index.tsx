3e9a9b1e5649acfbc622136ce6fe198f6b24898c
Iann-rst <iannrst11@gmail.com>
2022-09-15 16:56:00 -0300

    Feat: Buscar as informações dos games no back-end

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,15 +1,27 @@
+import { useEffect, useState } from 'react';
 import { Image, View, FlatList } from 'react-native';
+
+import logoImg from '../../assets/logo-nlw-esports.png';
+import { Heading } from '../../components/Heading';
+import { GameCard, GameCardProps } from '../../components/GameCard';
+
 
 import { styles } from './styles';
 
 
-import logoImg from '../../assets/logo-nlw-esports.png';
-import { Heading } from '../../components/Heading';
-import { GameCard } from '../../components/GameCard';
-
-import { GAMES } from '../../utils/games';
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+
+
+
+  useEffect(() => {
+    fetch('http://10.0.0.133:3333/games')
+      .then(response => response.json())
+      .then(data => setGames(data))
+
+  }, [])
+
   return (
     <View style={styles.container}>
       <Image
@@ -21,7 +33,7 @@ export function Home() {
 
       <FlatList
         contentContainerStyle={styles.contentList}
-        data={GAMES}
+        data={games}
         keyExtractor={item => item.id}
         renderItem={({ item }) => (<GameCard data={item} />)}
         horizontal
